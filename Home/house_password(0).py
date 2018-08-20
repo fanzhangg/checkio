1bee478e162f7fea64d92d96188cc72d4fc312d6
Fan_Zhang <vanadiumzhang@gmail.com>
2018-08-20 23:22:48 +0800

    Update 1.0

--- Home/house_password(0).py
+++ Home/house_password(0).py
@@ -3,16 +3,12 @@ def checkio(data: str) -> bool:
     result = True
 
     if len(data) < 10:
-        # print("The length of the password is less than 10 symbols")
         result = False
     if not any(i.isdigit() for i in data):
-        # print("The password does not contain any digit")
         result = False
     if not any(i.isupper() for i in data):
-        # print("The password does not contain any uppercase letter")
         result = False
     if not all(i.isdigit() or i.isalpha() for i in data):
-        # print("The password does not contain only ASCII latin letters or digits")
         result = False
     return result
 
